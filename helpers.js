0426c001134feb944212835d9ebf5f65a2c422ab
Gavin Rehkemper <gavin@gavinr.com>
2023-03-23 23:33:42 -0500

    chore: code formatting, readme, and package updates

--- helpers.js
+++ helpers.js
@@ -1,20 +1,25 @@
 const createIssue = (octokit, issueInfo, organization, repository) => {
   return new Promise((resolve, reject) => {
-    octokit.request("POST /repos/" + organization + "/" + repository + "/import/issues", issueInfo).then(
-      (res) => {
-        // console.log("res", res);
-        if (res.status === 202) {
+    octokit
+      .request(
+        "POST /repos/" + organization + "/" + repository + "/import/issues",
+        issueInfo
+      )
+      .then(
+        (res) => {
+          // console.log("res", res);
+          if (res.status === 202) {
             console.log(`Imported issue: ${issueInfo.issue.title}`);
             resolve(res);
-        } else {
-          // error creating the issue
-          reject(res);
+          } else {
+            // error creating the issue
+            reject(res);
+          }
+        },
+        (err) => {
+          reject(err);
         }
-      },
-      (err) => {
-        reject(err);
-      }
-    );
+      );
   });
 };
 
